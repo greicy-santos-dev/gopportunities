571124dbe87bab25f50ec674b32a60570c49a38f
greicy-santosProgrammer <greicy.santos.programmer@gmail.com>
2025-09-24 20:18:11 -0300

    feat update handler structure to separate package

---
+++ handler/handler.go
@@ -0,0 +1,29 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func CreateOpeningHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"msg": "POST Opening",
+	})
+}
+func ShowOpeningHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"msg": "GET Opening",
+	})
+}
+func DeleteOpeningHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"msg": "DELETE Opening",
+	})
+}
+func UpdateOpeningHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"msg": "UPDATE Opening",
+	})
+}
+func ListOpeningsHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"msg": "GET Openings",
+	})
+}
--- router/routes.go
+++ router/routes.go
@@ -1,34 +1,17 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/greicy-santos-dev/gopportunities/handler"
+)
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"msg": "GET Opening",
-			})
-		})
-		v1.POST("/opening", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"msg": "POST Opening",
-			})
-		})
-		v1.DELETE("/opening", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"msg": "DELETE Opening",
-			})
-		})
-		v1.PUT("/opening", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"msg": "PUT Opening",
-			})
-		})
-		v1.GET("/openings", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"msg": "GET Openings",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
